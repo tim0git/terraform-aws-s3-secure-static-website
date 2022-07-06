93c193ca134aa2c05abb4eb709310ca29bdd3349
Timothy Doolan <tim0git>
2022-07-06 20:27:17 +0100

    fix: argument named enabled is not expected here in aws_s3_bucket_versioning block

--- main.tf
+++ main.tf
@@ -25,7 +25,6 @@ resource "aws_s3_bucket" "s3_bucket" {
 
 resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
   bucket  = aws_s3_bucket.s3_bucket.id
-  enabled = true
   versioning_configuration {
     mfa_delete = false
     status     = "Enabled"
