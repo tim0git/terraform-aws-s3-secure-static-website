bf7c3fd06f54c51bd6504bf144c8ea4c6e7c7997
Timothy Doolan <tim0git>
2022-07-06 19:13:30 +0100

    refactor: basic example to use uuid for domain name to ensure that bucket name space collisions do not occur under test.

--- Examples/Basic/main.tf
+++ Examples/Basic/main.tf
@@ -2,10 +2,13 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+resource "random_uuid" "test" {
+} // required for test only
+
 module "cloudfront_s3_website_with_default_domain" {
   source                 = "../../"
   version                = "1.0.0"
-  domain_name            = "test-application-1232" // random identifier for s3 bucket name
+  domain_name            = "test-${random_uuid.test.result}" //  random identifier for s3 bucket name
   use_default_domain     = true
   upload_sample_file     = true
 }
